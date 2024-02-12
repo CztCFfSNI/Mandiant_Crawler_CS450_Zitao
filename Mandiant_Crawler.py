2c10e1e6b2cf18df1e9a93da1d11b53b9ff5c1a5
cai.851 <cai.851@osu.edu>
2024-02-12 13:17:10 -0600

    Update the crawler.

--- Mandiant_Crawler.py
+++ Mandiant_Crawler.py
@@ -11,17 +11,51 @@ def crawl_and_save_blog_content(url, file_path):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(content)
 
-base_url = 'https://www.mandiant.com/resources/blog'
-response = requests.get(base_url)
-soup = BeautifulSoup(response.content, 'html.parser')
+def crawl_and_save_blog_content_with_data_cleaning(url, file_path):
+    resp = requests.get(url)
+    page_soup = BeautifulSoup(resp.content, 'html.parser')
+    figures, codes, paragraphs, unique_set = page_soup.find_all('img'), page_soup.find_all(['pre', 'code']), page_soup.find_all(['p', 'span']), set()
+    with open(file_path, 'w', encoding='utf-8') as file:
+        if codes:
+            file.write("CODES:\n")
+            for code in codes:
+                if code.get_text() not in unique_set:
+                    file.write(f"{code.get_text()}\n")
+                    unique_set.add(code.get_text())
+            file.write("\n")
 
-blog_links = soup.find_all('a', class_='resources-card')
+        if paragraphs:
+            file.write("PARAGRAPHS:\n")
+            for paragraph in paragraphs:
+                paragraph_text = paragraph.get_text(strip=True)
+                if paragraph_text not in unique_set:
+                    unique_set.add(paragraph_text)
+                    file.write(f"{paragraph_text}\n")
+            file.write("\n")
 
+        # if figures:
+        #     file.write("FIGURES:\n")
+        #     for figure in figures:
+        #         alt_text = figure.get('alt', 'No alt text available')
+        #         file.write(f"Figure: {alt_text}\n")
+        #     file.write("\n")
+
+# 594 blogs in total.
 os.makedirs('mandiant_blogs', exist_ok=True)
+index, page_number_start, page_number_end = 0, 0, 59
 
-for i, link in enumerate(blog_links, start=1):
-    blog_url = urljoin(base_url, link['href'])
-    blog_title = link.get_text(strip=True)
-    file_name = f"mandiant_blog_{i}_{blog_title}.txt".replace("/", "_").replace(" ", "_")
-    file_path = os.path.join('mandiant_blogs', file_name)
-    crawl_and_save_blog_content(blog_url, file_path)
+for page_number in range(page_number_start, page_number_end + 1):
+    base_url = 'https://www.mandiant.com/resources/blog'
+    page_url = f'{base_url}?page={page_number}'
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    blog_links = soup.find_all('a', class_='resources-card')
+
+    for _, link in enumerate(blog_links, start=1):
+        blog_url = urljoin(base_url, link['href'])
+        blog_title = link.get_text(strip=True)
+        file_name = f"mandiant_blog_{index}.txt".replace("/", "_").replace(" ", "_")
+        file_path = os.path.join('mandiant_blogs', file_name)
+        # crawl_and_save_blog_content(blog_url, file_path)
+        crawl_and_save_blog_content_with_data_cleaning(blog_url, file_path)
+        index += 1
